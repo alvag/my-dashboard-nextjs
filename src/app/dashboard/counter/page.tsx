9aeb9693ac4490e61503dd5674e0a7f0cd8bf05f
Max Alva Garcia <alva85@gmail.com>
2023-06-24 14:31:38 -0500

    Counter y manejo de estados

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,23 @@
-export default function CounterPage() {
+import { FC } from 'react';
+import { CartCounter } from '@/app/shopping-cart';
+
+export const metadata = {
+    title: 'Counter',
+    description: 'Counter page',
+}
+
+interface CounterProps {
+}
+
+const CounterPage: FC<CounterProps> = ( {} ) => {
+
     return (
-        <div>
-            CounterPage
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos en el carrito de compras</span>
+
+            <CartCounter value={ 15 }/>
         </div>
     );
 };
+
+export default CounterPage;
